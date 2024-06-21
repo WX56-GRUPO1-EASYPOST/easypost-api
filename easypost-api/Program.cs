dd4437e880a9f695c68687e6e14c0f53338451fa
JorgeQuilla <u20211b197@upc.edu.pe>
2024-06-21 17:00:58 -0500

    chore: cors configuration

--- easypost-api/Program.cs
+++ easypost-api/Program.cs
@@ -93,6 +93,16 @@ builder.Services.AddSwaggerGen(c =>
 // Configure Lowercase URLs
 builder.Services.AddRouting(options=>options.LowercaseUrls=true);
 
+//Configure CORS Service
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllPolicy", policy =>
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+
 // Configure Dependency Injection
 
 // Shared Bounded Context Injection Configuration
@@ -174,35 +184,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAllPolicy");
+
 app.UseHttpsRedirection();
-
-/*var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();*/
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
-
-/*record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}*/
